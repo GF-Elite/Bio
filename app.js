8ecf3808190e71e4bbad5b34958e8845c7b17163
GF-Elite <litageming@gmail.com>
2023-06-23 02:00:39 +0700

    data

---
+++ app.js
@@ -0,0 +1,4 @@
+// Menggunakan event listener untuk menghentikan klik kanan pada halaman
+document.addEventListener('contextmenu', function(event) {
+  event.preventDefault(); // Menghentikan tindakan default menu konteks
+});
